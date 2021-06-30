c20136ca3a1e7cb19a1c9ca9d656359520752186
James Kozianski <jkozianski@sajari.com>
2021-06-30 17:39:36 +1000

    add protos

--- foo/cmd/foo/main.go
+++ foo/cmd/foo/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	pb "go.sajari.com/bazeltest/foo/cmd/foo/v1"
+)
 
 func main() {
 	fmt.Println("vim-go")
+	fmt.Printf("%v\n", (&pb.Hello{
+		Name: "hi",
+	}).GetName())
 }
